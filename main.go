c0b2d31bd21f61a14f62836d3ab9095b7381bce7
chapristi <70172942+chapristi@users.noreply.github.com>
2022-02-11 21:41:42 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,55 +3,63 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
-	"strconv"
+	"strings"
 )
 
-const (
-	tailleDamier   = 9
-	symboleJoueur1 = "X"
-	symboleJoueur2 = "O"
-)
+func ProcessLine(line, old, new string) (found bool, res string, occ int) {
+	//sera appelé par findreplace a chaque ligne lu
+	//ligne à traiter
+	//old ancien mot
+	// new nouveau mot
+	//found vrai ou faux si au moins une occurence de trouvé
+	//res resultat de la ligne
+	//occ nombre d'occurence de old
+	isWord := strings.Contains(line, old) // repere si y a un mot dans la ligne
+	if isWord {
+		nbr := strings.Count(line, old)                  //contient le nombre d'occurence
+		res := strings.Replace(line, "go", "python", -1) //remplace
+		return isWord, res, nbr
+	}
+	return false, line, 0
 
-var (
-	tableauMorpion = [tailleDamier]string{
-		"1", "2", "3",
-		"4", "5", "6",
-		"7", "8", "9"}
-	player1 bool = true
-)
+}
+func FindReplaceFile(src string, old, new string) (occ int, lines []int, err error) {
+	//src le lien du fichier,
+	//old l'ancien mot
+	//new le nouveau mot
+	//occ nombre d'occurence
+	//lines les lignes ou old a etait trouvé
+	//err erreur de la fonction
 
-func Play() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for true {
-		var index string
-
-		if player1 == true {
-			index = symboleJoueur1
-		} else {
-			index = symboleJoueur2
+	file, err := os.Open(src)
+	if err != nil {
+		log.Fatalf("Error when opening file: %s", err)
+	}
+	scanner := bufio.NewScanner(file)
+	count := 1
+	for scanner.Scan() {
+		count += 1
+		t := scanner.Text() //contient une ligne
+		isFound, res, nbr := ProcessLine(t, old, new)
+		if isFound {
+			lines[count-1] = count
+			occ += nbr
+			t = res
 		}
-		scanner.Scan()
-		var text string
-		text = scanner.Text()
-		if strconv.Atoi(text)-1 > 9 || strconv.Atoi(text)-1 < 1 {
-			tableauMorpion[strconv.Atoi(text)-1] = index
-			if tableauMorpion[strconv.Atoi(text)-1] != "X" || "O" {
-				tableauMorpion[strconv.Atoi(text)-1] = index
-				if player1 == true {
-					player1 = false
-				} else {
-					player1 = true
-				}
-			}
-		}
-		continue
 
 	}
+	return occ, lines, err
 
 }
 
 func main() {
-	fmt.Printf("test : ", tableauMorpion[0])
-	fmt.Println("Hello World")
+	occ, lines, err := FindReplaceFile("i.txt", "go", "python")
+	if err != nil {
+		log.Fatalf("Error when opening file: %s", err)
+	}
+	fmt.Println("les lignes", lines)
+	fmt.Println("nombre d'occurence", occ)
+
 }
